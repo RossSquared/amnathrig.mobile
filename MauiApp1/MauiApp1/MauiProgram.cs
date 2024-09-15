50b3db9db3cec86b560aa09e2c8a83d14ffb1e34
dalevross <dalevross@gmail.com>
2024-09-15 12:13:09 -0400

    Modified to use log in to dev server

---
+++ MauiApp1/MauiApp1/MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿using IdentityModel.OidcClient;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp1;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        // setup OidcClient
+        builder.Services.AddSingleton(new OidcClient(new()
+        {
+            Authority = "https://dev.amnathrig.app",
+
+            ClientId = "maui_client",
+            Scope = "openid profile amnathrigAPI",
+            RedirectUri = "amnathrig://callback",
+
+            Browser = new MauiAuthenticationBrowser()
+        }));
+
+        // add main page
+        builder.Services.AddSingleton<MainPage>();
+
+        return builder.Build();
+    }
+}
